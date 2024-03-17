0e3a99a666304b586d0e6284084bbdf8effd6221
Muhammad Rehan <145863068+muhammadrehan00@users.noreply.github.com>
2024-03-17 11:49:23 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function rollDice() {
+    const randomNumber = Math.ceil(Math.random() * 6);
+
+    const diceImageElement = document.getElementById("dice-image");
+
+    diceImageElement.src = "./images/dice " + randomNumber + ".jpg";
+
+}
